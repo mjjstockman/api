1284f366e518c4b112906f33346308f5aa1675b5
mjjstockman <mjjstockman@gmail.com>
2021-12-10 13:26:27 +0000

    Finish POST request

--- assets/js/script.js
+++ assets/js/script.js
@@ -48,12 +48,24 @@ async function postForm(e) {
 }
 
 function displayErrors(data) {
-    // set the heading
-    let heading = `JSHint Results for ${data.file}`;
 
+    let results = "";
+
+    let heading = `JSHint Results for ${data.file}`;
     if (data.total_errors === 0) {
-        results = `<div>Total Errors:`
+        results = `<div class="no_errors">No errors reported!</div>`;
+    } else {
+        results = `<div>Total Errors: <span class="error_count">${data.total_errors}</span></div>`;
+        for (let error of data.error_list) {
+            results += `<div>At line <span class="line">${error.line}</span>, `;
+            results += `column <span class="column">${error.col}:</span></div>`;
+            results += `<div class="error">${error.error}</div>`;
+        }
     }
+
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+    resultsModal.show();
 }
 
 // qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq
